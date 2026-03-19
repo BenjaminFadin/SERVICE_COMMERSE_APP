70e830b57b838c781410418c9d23283bd74efc46
Benjamin Dusse <benj@gmail.com>
2026-03-19 15:34:19 +0500

    current state pre server

--- config/settings.py
+++ config/settings.py
@@ -90,25 +90,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Template for PostgreSQL database configuration
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "service_commerse_db",
-#         "USER": "postgres",
-#         "PASSWORD": config.get('DB_PASSWORD'),
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "service_commerse_db",
+        "USER": "postgres",
+        "PASSWORD": config.get('DB_PASSWORD'),
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
