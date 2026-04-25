18a07e39f417c347da9d1e502879cc886984c430
Benjamin Dusse <benj@gmail.com>
2026-04-25 22:33:28 +0500

    domain setting

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,7 @@ config = dotenv_values(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DOMAIN_NAME = config.get('DOMAIN_NAME')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -154,8 +155,8 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = f'/var/www/{DOMAIN_NAME}/static/'
+MEDIA_ROOT = f'/var/www/{DOMAIN_NAME}/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
